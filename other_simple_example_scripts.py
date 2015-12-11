90da22ac035ee4090c8c2307ceb3746ad0fc44cb
aemann <allison.e.mann@gmail.com>
2015-12-11 16:46:12 -0600

    New repo

---
+++ other_simple_example_scripts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import random
+
+#super simple while loop
+print "While loop example"
+count = 100
+
+while count:
+	print "Value of count is now: %i" %count
+	count -= 1
+
---
+++ otu_table_fix.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+import sys #library that allows you to communicate via command line
+import pandas as pd #load and set up alias for pandas library
+
+"""PROBLEM: you have a tab delimited OTU table called filtered_otu_table_L6.txt with redundant taxa strings that need to be collapsed
+"""
+
+#first include some color text class variables (purely bells and whistles)
+class colors:
+	COMPLETE = '\033[92m'
+
+data = pd.read_csv(sys.argv[1], sep="\t", skiprows=1)
+fixed_data = data.groupby("#OTU ID").sum()
+
+#how many duplicate records were in the file?
+
+duplicates = data.shape[0] - fixed_data.shape[0]
+print "Number of duplicate rows to be collapsed: %i" %duplicates
+
+#now write your fixed OTU table to file
+
+with open("collapsed_otu_table.txt", "w") as outfile:
+	fixed_data.to_csv(outfile, sep="\t")
+
+outfile.close() #you're done writing to file, close it (not necessary but good practice)
+
+print colors.COMPLETE + "Complete, collapsed OTU table written to: collapsed_otu_table.txt" #tell the user that it's done!
+
